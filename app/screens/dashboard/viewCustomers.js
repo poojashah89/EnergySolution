0234e9bdf657831054f406afe457b0f85769977d
Sneha Kasetty Sudarshan <sneha@Snehas-MacBook-Pro.local>
2017-12-06 02:50:22 -0800

    get call implemented to Admin View customers

--- app/screens/dashboard/viewCustomers.js
+++ app/screens/dashboard/viewCustomers.js
@@ -2,11 +2,14 @@ import React from 'react';
 import * as Screens from '../../screens/index';
 import {
   ScrollView,
+  FlatList,
+  Image,
   View,
   TouchableOpacity,
   StyleSheet
 } from 'react-native';
 import {
+  RkCard,
   RkButton,
   RkText,
   RkStyleSheet,
@@ -18,12 +21,21 @@ import {
   GradientButton
 } from '../../components';
 import {FontAwesome} from '../../assets/icons';
+import {
+  ProgressChart,
+  DoughnutChart,
+  AreaChart,
+  AreaSmoothedChart,
+  Avatar,
+  SocialBar,
+  PaginationIndicator
+} from '../../components/';
 
 export class ViewCustomers extends React.Component {
   static navigationOptions = {
     title: 'View Customers'.toUpperCase()
   };
-
+/*
   constructor(props) {
     super(props);
 
@@ -34,53 +46,86 @@ export class ViewCustomers extends React.Component {
       googleEnabled: false,
       facebookEnabled: true
     }
-  }
+  } */
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        <View style={styles.section}>
-          <View style={[styles.row, styles.heading]}>
-            <RkText rkType='primary header6'>CUSTOMERID                                   NAME</RkText>
-          </View>
-          <View style={styles.row}>
-            <TouchableOpacity style={styles.rowButton}>
-              <RkText rkType='header6'>1                                                         SNEHA</RkText>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.row}>
-            <TouchableOpacity style={styles.rowButton}>
-              <RkText rkType='header6'>2                                                         SINDHYA</RkText>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.row}>
-          <TouchableOpacity style={styles.rowButton}>
-            <RkText rkType='header6'>3                                                         POOJA</RkText>
-
-              </TouchableOpacity>
-          </View>
-          <View style={styles.row}>
-          <TouchableOpacity style={styles.rowButton}>
-            <RkText rkType='header6'>4                                                         MOHAMMAD</RkText>
-              </TouchableOpacity>
-          </View>
-          <View style={styles.row}>
-
-        </View>
-
-        <View style={styles.content}>
-          <View>
-            <GradientButton style={styles.save} rkType='large' text='Back' onPress={() => {
-              this.props.navigation.goBack()
-            }}/>
-          </View>
-        </View>
-
-        </View>
-      </ScrollView>
-    )
-  }
+  constructor(props) {
+  super(props);
+  //1. call the function to get the list of items
+  this.getServices();
+  this.pic = 'https://www.servicechampions.net/wp-content/uploads/2015/03/air-ducts-energy-star.jpg';//'https://static1.squarespace.com/static/58588d72e6f2e1e1d54aa8e4/t/5876d0da03596ef4262a5362/1484181726567/Icon+Clean+Tech+darkgreen.png';
+  //2.Define the array to store the list
+  this.state = {
+     serviceList : []
+  };
+  //3. bind renderItem to call _renderItem function to render each item in the list
+ this.renderItem = this._renderItem.bind(this);
 }
+
+//4. write the function to get the list from backend
+getServices = () => {
+    console.log("Inside getServices");
+    fetch('https://cmpe235-finalproject.herokuapp.com/v1/user', {
+       method: 'GET'
+    })
+    .then(response => {
+      console.log("response:"+JSON.stringify(response));
+      var obj= {};
+      //parse the response - get response from response._bodyInit
+      obj= JSON.parse(JSON.stringify(response));
+      var bodyInit = JSON.parse(obj._bodyInit);
+      this.obj = bodyInit.obj;
+      var item = {};
+      item = JSON.stringify(this.obj);
+      console.log("response1:"+ JSON.stringify(item));
+      console.log("response obj"+item.id);
+
+      //5. set the array from the response to the list
+      this.setState({
+        //isLoading: false,
+        serviceList : this.obj
+      });
+
+    })
+    .catch((error) => {
+       console.error(error);
+    });
+ }
+
+   _renderItem(info) {
+    return (
+
+     <ScrollView style={styles.container}>
+
+       <View style={styles.section}>
+         <View style={styles.row}>
+          <RkText  rkType='header6'>{info.item.name}</RkText>
+          <RkText  rkType='header6'>{info.item.email}</RkText>
+        </View>
+       </View>
+     </ScrollView>
+  )
+}
+
+
+render() {
+  return (
+    <View>
+    <View style={styles.container}>
+      <RkText rkType='primary header6'>    NAME                                     EMAIL</RkText>
+    </View>
+    <View>
+      <FlatList
+      //6. Set the data for the flat list
+        data={this.state.serviceList}
+        renderItem={this.renderItem}
+        keyExtractor={(item, index) => item._id}
+        style={styles.container}/>
+    </View>
+  </View>
+  )
+}
+}
+
 
 let styles = RkStyleSheet.create(theme => ({
   container: {
